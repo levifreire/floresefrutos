a42d8a963c3e72504baa3cedeef9170ef9ffdf8d
Levi Freire <levifreireoliveira@hotmail.com>
2019-07-24 14:00:03 -0300

    adicionado as configurações de saida no webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,11 @@ const webpack = require('webpack')
 module.exports = {
     mode: 'development',
     //definindo a entrada das chamadas de import
-    entry: './src/main.js'
+    entry: './src/main.js',
+    //pasta de saida
+    output: {
+        filename:'principal.js',
+        //pasta de destino dos arquivos gerados
+        path: __dirname + '/public'
+    }
 }
